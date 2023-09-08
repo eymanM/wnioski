d28d417e0b17c8801f4e7474f3c8320eb5f1f783
Mateusz Stefanowicz <mateusz.stefanowicz@utter.one>
2023-09-08 17:39:40 +0200

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const {nextui} = require("@nextui-org/react");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      visibility: ['group-hover'],
+    },
+  },
+  plugins: [require('@tailwindcss/typography'), nextui()],
+};
---
+++ types/chat.ts
@@ -0,0 +1,21 @@
+import { OpenAIModel } from './openai';
+
+export interface Message {
+  role: Role;
+  content: string;
+}
+
+export type Role = 'assistant' | 'user';
+
+export interface ChatBody {
+  messages: Message[];
+  key: string;
+  prompt: string;
+}
+
+export interface Conversation {
+  id: string;
+  name: string;
+  messages: Message[];
+  prompt: string;
+}
